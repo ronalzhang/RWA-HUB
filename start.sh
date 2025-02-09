d4f08c79221226961cd291613028772b040d9a0c
ronalzhang <jiaming0427@gmail.com>
2025-02-10 02:46:55 +0800

    使用 Selenium 获取动态内容

---
+++ start.sh
@@ -0,0 +1,1 @@
+source .venv/bin/activate && DATABASE_URL="postgresql://rwa_hub_user:3YIeu6i1Nuyb6z8wRAxdctbMJVrSseJB@dpg-cu6b270gph6c73c50eag-a/rwa_hub" FLASK_ENV="production" python run.py --port 10000
