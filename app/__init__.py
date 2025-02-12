0592fc6bf9b4f0cb8b9e781fc0a3bba77924d0e7
ronalzhang <jiaming0427@gmial.com>
2025-02-13 00:14:33 +0800

    fix: 简化 Flask-Babel 初始化

--- app/__init__.py
+++ app/__init__.py
@@ -37,10 +37,6 @@ def create_app(config_name='development'):
     app.config['BABEL_SUPPORTED_LOCALES'] = ['en', 'zh_Hant']
     babel.init_app(app)
     
-    @babel.localeselector
-    def get_locale():
-        return request.cookies.get('language', 'en')
-    
     # 确保日志目录存在
     if not os.path.exists('logs'):
         os.makedirs('logs')
